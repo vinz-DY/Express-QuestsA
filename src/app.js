5e7a8dcdae98719cb8ca81387b84cafe70021742
vinz-DY <vincentducamp259@hotmail.com>
2023-11-02 16:40:01 +0100

    ajout sebastien baquet via la route post et ajouté par postman

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json()); 
+
 const movieControllers = require("../movieControllers.js");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postUser);
 
 module.exports = app;
